a2e2170d701de420468415be96d930d4ff55753c
Ben <andreabenini@users.noreply.github.com>
2022-04-12 00:04:11 +0200

    script test unit update

--- test.run.sh
+++ test.run.sh
@@ -9,17 +9,41 @@ catch() {
 }
 # Tests
 mkdir -p ./tests/output
+rm -rf ./tests/output/*
 if [ $(ls -1 tests|wc -l) -lt 2 ]; then echo "Copy some sample data into './tests' to continue"; exit 1; fi
-echo -e "\n\033[1;34mSTARTING TESTS\033[0;37m"
+echo -e "\n\033[1;97mSTARTING TESTS"
 
 
-echo "- Splitting README.md by header 1"
-echo "  "./mdmt --input=./tests/README.md --output=./tests/output/ --template=./templates/mdmt.css --header-split 1
+echo -e "\033[0;96m- Conversion  README.md -> README.1.html\033[0;90m"
+echo -e "  "./mdmt --input=tests/README.md --output=tests/output/README.1.html
+./mdmt --input=tests/README.md --output=tests/output/README.1.html |grep -v WARNING
+
+echo -e "\033[0;96m- Conversion  README.md -> README.2.html [TEMPLATE]\033[0;90m"
+echo -e "  "./mdmt --input=tests/README.md --output=tests/output/README.2.html --template=templates/mdmt.css
+./mdmt --input=tests/README.md --output=tests/output/README.2.html --template=templates/mdmt.css
+
+echo -e "\033[0;96m- Conversion  README.md -> README.1.pdf\033[0;90m"
+echo -e "  "./mdmt --input=tests/README.md --output=tests/output/README.1.pdf
+./mdmt --input=tests/README.md --output=tests/output/README.1.pdf |grep -v WARNING
+
+echo -e "\033[0;96m- Conversion  README.md -> README.2.pdf  [TEMPLATE]\033[0;90m"
+echo -e "  "./mdmt --input=tests/README.md --output=tests/output/README.2.pdf --template=templates/mdmt.css
+./mdmt --input=tests/README.md --output=tests/output/README.2.pdf --template=templates/mdmt.css
+
+echo -e "\033[0;96m- Joining     input-list-> README.3.pdf  [TEMPLATE]\033[0;90m"
+echo    '  cat << EOT | ./mdmt --input-list - --output=tests/output/README.3.pdf --template=templates/mdmt.css'
+cat << EOT  | ./mdmt --input-list - --output=tests/output/README.3.pdf --template=templates/mdmt.css
+tests/README.md
+tests/SEC-RES-REQ-1.md
+EOT
+
+echo -e "\033[0;96m- Splitting   README.md by header 1\033[0;90m"
+echo -e "  "./mdmt --input=./tests/README.md --output=./tests/output/ --template=./templates/mdmt.css --header-split 1
 ./mdmt --input=./tests/README.md --output=./tests/output/ --template=./templates/mdmt.css --header-split 1
 
-#echo "- Splitting README.md by header 2"
-#echo "  "./mdmt --input=./tests/README.md --output=./tests/output/ --template=./templates/mdmt.css --header-split 2
-#./mdmt --input=./tests/README.md --output=./tests/output/ --template=./templates/mdmt.css --header-split 2
+# echo -e "\033[0;96m- Splitting   README.md by header 2\033[0;90m"
+# echo -e "  "./mdmt --input=./tests/README.md --output=./tests/output/ --template=./templates/mdmt.css --header-split 2
+# ./mdmt --input=./tests/README.md --output=./tests/output/ --template=./templates/mdmt.css --header-split 2
 
 
-echo -e "\n\033[0;32mCOMPLETED\n"
+echo -e "\n\033[0;32m- - - -   COMPLETED   - - - -\033[0;37m\n"
