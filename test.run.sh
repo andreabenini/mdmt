6d4d0cad552e3082c6cb9fc8a170321b4f0213ab
Ben <andreabenini@users.noreply.github.com>
2022-04-17 01:54:53 +0200

    Testing headers splitting

--- test.run.sh
+++ test.run.sh
@@ -14,28 +14,28 @@ if [ $(ls -1 tests|wc -l) -lt 2 ]; then echo "Copy some sample data into './test
 echo -e "\n\033[1;97mSTARTING TESTS"
 
 
-echo -e "\033[0;96m- Conversion  README.md -> README.1.html\033[0;90m"
-echo -e "  "./mdmt --input=tests/README.md --output=tests/output/README.1.html
-./mdmt --input=tests/README.md --output=tests/output/README.1.html |grep -v WARNING
+# echo -e "\033[0;96m- Conversion  README.md -> README.1.html\033[0;90m"
+# echo -e "  "./mdmt --input=tests/README.md --output=tests/output/README.1.html
+# ./mdmt --input=tests/README.md --output=tests/output/README.1.html |grep -v WARNING
 
-echo -e "\033[0;96m- Conversion  README.md -> README.2.html [TEMPLATE]\033[0;90m"
-echo -e "  "./mdmt --input=tests/README.md --output=tests/output/README.2.html --template=templates/mdmt.css
-./mdmt --input=tests/README.md --output=tests/output/README.2.html --template=templates/mdmt.css
+# echo -e "\033[0;96m- Conversion  README.md -> README.2.html [TEMPLATE]\033[0;90m"
+# echo -e "  "./mdmt --input=tests/README.md --output=tests/output/README.2.html --template=templates/mdmt.css
+# ./mdmt --input=tests/README.md --output=tests/output/README.2.html --template=templates/mdmt.css
 
-echo -e "\033[0;96m- Conversion  README.md -> README.1.pdf\033[0;90m"
-echo -e "  "./mdmt --input=tests/README.md --output=tests/output/README.1.pdf
-./mdmt --input=tests/README.md --output=tests/output/README.1.pdf |grep -v WARNING
+# echo -e "\033[0;96m- Conversion  README.md -> README.1.pdf\033[0;90m"
+# echo -e "  "./mdmt --input=tests/README.md --output=tests/output/README.1.pdf
+# ./mdmt --input=tests/README.md --output=tests/output/README.1.pdf |grep -v WARNING
 
-echo -e "\033[0;96m- Conversion  README.md -> README.2.pdf  [TEMPLATE]\033[0;90m"
-echo -e "  "./mdmt --input=tests/README.md --output=tests/output/README.2.pdf --template=templates/mdmt.css
-./mdmt --input=tests/README.md --output=tests/output/README.2.pdf --template=templates/mdmt.css
+# echo -e "\033[0;96m- Conversion  README.md -> README.2.pdf  [TEMPLATE]\033[0;90m"
+# echo -e "  "./mdmt --input=tests/README.md --output=tests/output/README.2.pdf --template=templates/mdmt.css
+# ./mdmt --input=tests/README.md --output=tests/output/README.2.pdf --template=templates/mdmt.css
 
-echo -e "\033[0;96m- Joining     input-list-> README.3.pdf  [TEMPLATE]\033[0;90m"
-echo    '  cat << EOT | ./mdmt --input-list - --output=tests/output/README.3.pdf --template=templates/mdmt.css'
-cat << EOT  | ./mdmt --input-list - --output=tests/output/README.3.pdf --template=templates/mdmt.css
-tests/README.md
-tests/SEC-RES-REQ-1.md
-EOT
+# echo -e "\033[0;96m- Joining     input-list-> README.3.pdf  [TEMPLATE]\033[0;90m"
+# echo    '  cat << EOT | ./mdmt --input-list - --output=tests/output/README.3.pdf --template=templates/mdmt.css'
+# cat << EOT  | ./mdmt --input-list - --output=tests/output/README.3.pdf --template=templates/mdmt.css
+# tests/README.md
+# tests/SEC-RES-REQ-1.md
+# EOT
 
 echo -e "\033[0;96m- Splitting   test.md by header 1\033[0;90m"
 echo -e "  "./mdmt --input=./tests/test.md --output=./tests/output/split --template=./templates/mdmt.css --header-split 1
