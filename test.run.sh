b0314b6d9602f7f7425872bd084afcba3fc0d146
Ben <andreabenini@users.noreply.github.com>
2022-04-20 00:17:06 +0200

    First beta version mermaid generation has bugs, basic features running fine

--- test.run.sh
+++ test.run.sh
@@ -30,6 +30,11 @@ echo -e "\n\033[1;97mSTARTING TESTS"
 # echo -e "  "./mdmt --input=tests/README.md --output=tests/output/README.2.pdf --template=templates/mdmt.css
 # ./mdmt --input=tests/README.md --output=tests/output/README.2.pdf --template=templates/mdmt.css
 
+echo -e "\033[0;96m- Conversion  test.mermaid.md -> test.mermaid.[html|pdf]  [TEMPLATE]\033[0;90m"
+echo -e "  "./mdmt --input=tests/test.mermaid.md --output=tests/output/test.mermaid.[html\|pdf] --template=templates/mdmt.css
+./mdmt --input=tests/test.mermaid.md --output=tests/output/test.mermaid.html --template=templates/mdmt.css
+./mdmt --input=tests/test.mermaid.md --output=tests/output/test.mermaid.pdf  --template=templates/mdmt.css
+
 # echo -e "\033[0;96m- Joining     input-list-> README.3.pdf  [TEMPLATE]\033[0;90m"
 # echo    '  cat << EOT | ./mdmt --input-list - --output=tests/output/README.3.pdf --template=templates/mdmt.css'
 # cat << EOT  | ./mdmt --input-list - --output=tests/output/README.3.pdf --template=templates/mdmt.css
@@ -37,17 +42,17 @@ echo -e "\n\033[1;97mSTARTING TESTS"
 # tests/SEC-RES-REQ-1.md
 # EOT
 
-echo -e "\033[0;96m- Splitting   test.md by header 1\033[0;90m"
-echo -e "  "./mdmt --input=./tests/test.md --output=./tests/output/split1 --template=./templates/mdmt.css --header-split 1
-rm -rf ./tests/output/split1
-mkdir -p ./tests/output/split1
-./mdmt --input=./tests/test.md --output=./tests/output/split1 --template=./templates/mdmt.css --header-split 1
+# echo -e "\033[0;96m- Splitting   test.md by header 1\033[0;90m"
+# echo -e "  "./mdmt --input=./tests/test.md --output=./tests/output/split1 --template=./templates/mdmt.css --header-split 1
+# rm -rf ./tests/output/split1
+# mkdir -p ./tests/output/split1
+# ./mdmt --input=./tests/test.md --output=./tests/output/split1 --template=./templates/mdmt.css --header-split 1
 
-echo -e "\033[0;96m- Splitting   README.md by header 2\033[0;90m"
-echo -e "  "./mdmt --input=./tests/test.md --output=./tests/output/split2 --template=./templates/mdmt.css --header-split 2
-rm -rf ./tests/output/split2
-mkdir -p ./tests/output/split2
-./mdmt --input=./tests/test.md --output=./tests/output/split2 --template=./templates/mdmt.css --header-split 2
+# echo -e "\033[0;96m- Splitting   README.md by header 2\033[0;90m"
+# echo -e "  "./mdmt --input=./tests/test.md --output=./tests/output/split2 --template=./templates/mdmt.css --header-split 2
+# rm -rf ./tests/output/split2
+# mkdir -p ./tests/output/split2
+# ./mdmt --input=./tests/test.md --output=./tests/output/split2 --template=./templates/mdmt.css --header-split 2
 
 
 echo -e "\n\033[0;32m- - - -   COMPLETED   - - - -\033[0;37m\n"
