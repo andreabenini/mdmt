5fa9cb4853c97ea8a9bbd762c32c85244105e746
Ben <andreabenini@users.noreply.github.com>
2022-04-22 00:55:47 +0200

    Complete test unit deploy

--- test.run.sh
+++ test.run.sh
@@ -1,5 +1,13 @@
 #!/usr/bin/env bash
-# "bash test unit"
+#
+# Bash test unit
+# @see      Yeah, I'm one of those folks that build test units all day, even for batch utilities :)
+#
+# @author   Ben <Andrea Benini>.  Email on github website for details
+# @see      https://github.com/andreabenini/mdmt
+#           Feel free to adapt to your environment,
+#             place your files into the test dir and you're ready to go
+#
 
 # Exit when a command fails
 set -e
@@ -14,45 +22,45 @@ if [ $(ls -1 tests|wc -l) -lt 2 ]; then echo "Copy some sample data into './test
 echo -e "\n\033[1;97mSTARTING TESTS"
 
 
-# echo -e "\033[0;96m- Conversion  README.md -> README.1.html\033[0;90m"
-# echo -e "  "./mdmt --input=tests/README.md --output=tests/output/README.1.html
-# ./mdmt --input=tests/README.md --output=tests/output/README.1.html |grep -v WARNING
+echo -e "\033[0;96m- Conversion  README.md -> README.1.html\033[0;90m"
+echo -e "  "./mdmt --input=tests/README.md --output=tests/output/README.1.html
+./mdmt --input=tests/README.md --output=tests/output/README.1.html |grep -v WARNING
 
-# echo -e "\033[0;96m- Conversion  README.md -> README.2.html [TEMPLATE]\033[0;90m"
-# echo -e "  "./mdmt --input=tests/README.md --output=tests/output/README.2.html --template=templates/mdmt.css
-# ./mdmt --input=tests/README.md --output=tests/output/README.2.html --template=templates/mdmt.css
+echo -e "\033[0;96m- Conversion  README.md -> README.2.html [TEMPLATE]\033[0;90m"
+echo -e "  "./mdmt --input=tests/README.md --output=tests/output/README.2.html --template=templates/mdmt.css
+./mdmt --input=tests/README.md --output=tests/output/README.2.html --template=templates/mdmt.css
 
-# echo -e "\033[0;96m- Conversion  README.md -> README.1.pdf\033[0;90m"
-# echo -e "  "./mdmt --input=tests/README.md --output=tests/output/README.1.pdf
-# ./mdmt --input=tests/README.md --output=tests/output/README.1.pdf |grep -v WARNING
+echo -e "\033[0;96m- Conversion  README.md -> README.1.pdf\033[0;90m"
+echo -e "  "./mdmt --input=tests/README.md --output=tests/output/README.1.pdf
+./mdmt --input=tests/README.md --output=tests/output/README.1.pdf |grep -v WARNING
 
-# echo -e "\033[0;96m- Conversion  README.md -> README.2.pdf  [TEMPLATE]\033[0;90m"
-# echo -e "  "./mdmt --input=tests/README.md --output=tests/output/README.2.pdf --template=templates/mdmt.css
-# ./mdmt --input=tests/README.md --output=tests/output/README.2.pdf --template=templates/mdmt.css
+echo -e "\033[0;96m- Conversion  README.md -> README.2.pdf  [TEMPLATE]\033[0;90m"
+echo -e "  "./mdmt --input=tests/README.md --output=tests/output/README.2.pdf --template=templates/mdmt.css
+./mdmt --input=tests/README.md --output=tests/output/README.2.pdf --template=templates/mdmt.css
 
 echo -e "\033[0;96m- Conversion  test.mermaid.md -> test.mermaid.[html|pdf]  [TEMPLATE]\033[0;90m"
 echo -e "  "./mdmt --verbose --input=tests/test.mermaid.md --output=tests/output/test.mermaid.[html\|pdf] --template=templates/mdmt.css
 ./mdmt --verbose --input=tests/test.mermaid.md --output=tests/output/test.mermaid.html --template=templates/mdmt.css
 ./mdmt --verbose --input=tests/test.mermaid.md --output=tests/output/test.mermaid.pdf  --template=templates/mdmt.css
 
-# echo -e "\033[0;96m- Joining     input-list-> README.3.pdf  [TEMPLATE]\033[0;90m"
-# echo    '  cat << EOT | ./mdmt --input-list - --output=tests/output/README.3.pdf --template=templates/mdmt.css'
-# cat << EOT  | ./mdmt --input-list - --output=tests/output/README.3.pdf --template=templates/mdmt.css
-# tests/README.md
-# tests/SEC-RES-REQ-1.md
-# EOT
+echo -e "\033[0;96m- Joining     input-list-> README.3.pdf  [TEMPLATE]\033[0;90m"
+echo    '  cat << EOT | ./mdmt --input-list - --output=tests/output/README.3.pdf --template=templates/mdmt.css'
+cat << EOT  | ./mdmt --input-list - --output=tests/output/README.3.pdf --template=templates/mdmt.css
+tests/README.md
+tests/SEC-RES-REQ-1.md
+EOT
 
-# echo -e "\033[0;96m- Splitting   test.md by header 1\033[0;90m"
-# echo -e "  "./mdmt --input=./tests/test.md --output=./tests/output/split1 --template=./templates/mdmt.css --header-split 1
-# rm -rf ./tests/output/split1
-# mkdir -p ./tests/output/split1
-# ./mdmt --input=./tests/test.md --output=./tests/output/split1 --template=./templates/mdmt.css --header-split 1
+echo -e "\033[0;96m- Splitting   test.md by header 1\033[0;90m"
+echo -e "  "./mdmt --input=./tests/test.md --output=./tests/output/split1 --template=./templates/mdmt.css --header-split 1
+rm -rf ./tests/output/split1
+mkdir -p ./tests/output/split1
+./mdmt --input=./tests/test.md --output=./tests/output/split1 --template=./templates/mdmt.css --header-split 1
 
-# echo -e "\033[0;96m- Splitting   README.md by header 2\033[0;90m"
-# echo -e "  "./mdmt --input=./tests/test.md --output=./tests/output/split2 --template=./templates/mdmt.css --header-split 2
-# rm -rf ./tests/output/split2
-# mkdir -p ./tests/output/split2
-# ./mdmt --input=./tests/test.md --output=./tests/output/split2 --template=./templates/mdmt.css --header-split 2
+echo -e "\033[0;96m- Splitting   README.md by header 2\033[0;90m"
+echo -e "  "./mdmt --input=./tests/test.md --output=./tests/output/split2 --template=./templates/mdmt.css --header-split 2
+rm -rf ./tests/output/split2
+mkdir -p ./tests/output/split2
+./mdmt --input=./tests/test.md --output=./tests/output/split2 --template=./templates/mdmt.css --header-split 2
 
 
 echo -e "\n\033[0;32m- - - -   COMPLETED   - - - -\033[0;37m\n"
